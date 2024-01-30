378735606b1c5384551cf5d2f2b282caebd56d6e
eyan <eyan.documet@protonmail.com>
2024-01-30 01:09:14 -0800

    Shuffled things out of assistant.py in an effort to make everything more Object-Oriented. Still need to create a proper skill object and intent-parsing.

--- assistant.py
+++ assistant.py
@@ -1,21 +1,35 @@
 import json
 import os
 from typing import List
-
-import pyaudio
-from pydub import AudioSegment
+from pydub.generators import Sine
 from pydub.playback import play
+import pyaudio
 import speech_recognition as sr
+
+
+def ping():
+    """
+    A somewhat annoying multipurpose chirp sound. Might get rid of this...
+    """
+    frequency = 850.5  # Hz
+    duration = 100  # ms
+
+    sine_wave = Sine(frequency)
+    tone = sine_wave.to_audio_segment(duration=duration)
+    play(tone)
+    play(tone)
+
 
 class Assistant:
     def __init__(self, wakewords: List[str]):
         self.wake_words = wakewords
         self.shutdown = False
 
+    # I/O, spoken word processing, and TTS
     @staticmethod
     def speak(text: str) -> None:
         """
-        Passes argument to a bash script located in the main folder.
+        Passes argument to a bash script located in the main folder. Currently using a piper-tts model for this.
         See speak.sh for details.
         """
         os.system(f'sh speak.sh \"{text}\"')
@@ -23,36 +37,83 @@ class Assistant:
     @staticmethod
     def listen():
         """
-        Listens for input using a VOSK model.
-        See model dir for details.
+        Listens for input using a VOSK model, then returns plain-text user input.
         """
         mic = pyaudio.PyAudio()
         stream = mic.open(format=pyaudio.paInt16, channels=1, rate=1600, input=True, frames_per_buffer=8192)
         stream.start_stream()
 
-        while True:
-            r = sr.Recognizer()
-            with sr.Microphone() as source:
-                print("Listening... ", end="")
-                audio = r.listen(source)
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Listening... ", end="")
+            audio = r.listen(source)
 
-                try:
-                    print("Recognizing... ", end="")
-                    query = r.recognize_vosk(audio, language='en-US')
-                    parsed_query = json.loads(query)[
-                        'text']  # Converts JSON -> dict -> plain-text; VOSK handles things weird...
+            try:
+                print("Recognizing... ")
+                query = r.recognize_vosk(audio, language='en-US')
+                parsed_query = json.loads(query)[
+                    'text']  # Converts JSON -> dict -> plain-text; VOSK handles things weird...
 
-                    print(f"User said: {parsed_query}")
+                print(f"User said: {parsed_query}")
 
-                except Exception as e:
-                    print("Exception: " + str(e))
+            except Exception as e:
+                print("Exception: " + str(e))
 
-            return parsed_query.lower()
+        return parsed_query.lower()
 
     def listen_for_wakeword(self):
-        audio_in = self.listen()
+        """
+        Self-explanatory: listens and identifies if a wakeword is present in the text. If it is, the whole command
+        gets passed back out.
+        """
+        input_string = self.listen()
         for word in self.wake_words:
-            if word in audio_in:
+            if word in input_string:
                 print("Wake word detected!")
-                ping()
-                return True, audio_in
+                return True, input_string
+
+    # Command execution, parsing, and logics
+    def process_command(self, command):
+        """
+        A bit hacky for now, but this essentially serves as a buffer between the intent recognizer and the
+        skill executor.
+        """
+        print("Processing... ", end="")
+        intended_skills = self.find_intents(command)
+        for skill in intended_skills:
+            reply = self.execute_skill(skill)
+            self.speak(reply)
+
+    def find_intents(self, command):
+        """
+        Parses through a dict to determine which keywords were uttered in the user command; returns list of
+        intended commands to be executed.
+        """
+        print("Determining intents... ", end="")
+        intents = {
+            ("hello", "how are you", "what's up"): "chit_chat",
+            ("who are you", "what do you do"): "introductions",
+        }
+
+        found_intents = [intent_name for keywords, intent_name in intents.items() if
+                         any(keyword in command for keyword in keywords)]
+
+        print(f"Intents: {found_intents}")
+        return found_intents
+
+    def execute_skill(self, skill):
+        # TODO: Create skill object, figure out logics involved (importing list of skills and associated keywords,
+        #  executing skills, etc.) Current system is a little too "hacky"
+        # TODO: Create some basic skills:
+        #   - Wikipedia
+        #   - Time and date
+        #   - Alarm
+        #   - DJ
+        #   - Therapy mode
+        print(f"Executing skill: {skill}")
+        if skill == 'chit_chat':
+            return 'Hello! I am well. How can I help you today?'
+        if skill == 'introductions':
+            return 'I am Carolyn, I am a text-to-speech virtual assistant designed to assit my users.'
+        else:
+            return 'I\'m sorry, I didn\'t catch that. Could you repeat it?'
--- main.py
+++ main.py
@@ -4,28 +4,16 @@ from assistant import Assistant, ping
 def main():
     # Init protocol
     wakewords = ["caroline", "carolyn", "karen", "computer"]
-    assistant = Assistant(wakewords)
-
+    a = Assistant(wakewords)
     ping()
-    assistant.speak(f"Carolyn online.")
 
     # Conversation loop; listens for wakeword, if wakeword present in text, parses for command and returns spoken
-    # output (or executes program).
-    while not assistant.shutdown:
-        alert, command = assistant.listen_for_wakeword()
+    # output (or executes program). To be improved.
+    while True:
+        alert, command = a.listen_for_wakeword()
         if alert:
-            assistant.speak(command)
-        # TODO: Create skill object, figure out logics involved (importing list of skills and associated keywords,
-        #  executing skills, etc.) Note: Perhaps we format the skills and keywords as a dict where the keys are a
-        #  list of keywords and the values are the skill objects.
-
-        # TODO: Create some basic skills:
-        #   - Wikipedia
-        #   - Time and date
-        #   - Alarm
-        #   - DJ
-        #   - Therapy mode
-
+            print(f"Command: {command}")
+            a.process_command(command)
 
 if __name__ == '__main__':
     main()
